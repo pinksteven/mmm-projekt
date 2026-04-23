803ad893af24e28451edd324ae1965fe11f99f14
PinkSteven <st.latuszek@gmail.com>
2026-04-23 10:51:28 +0200

    generatory

---
+++ src/generators.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def harmonic(amplitude, frequency, time, dc_offset=0):
+    return 0.5 * amplitude * np.sin(2 * np.pi * frequency * time) + dc_offset
+
+
+def square_wave(amplitude, frequency, time, fill=0.5, dc_offset=0):
+    period = 1 / frequency
+    if (time % period) <= fill * period:
+        return 0.5 * amplitude + dc_offset
+    else:
+        return -0.5 * amplitude + dc_offset
+
+
+def impulse(high_state, len, start_t, time, dc_offset=0):
+    if start_t <= time <= start_t + len:
+        return high_state + dc_offset
+    else:
+        return dc_offset
+
+
+def triangle_wave(amplitude, frequency, time, dc_offset=0):
+    period = 1 / frequency
+    t = (time % period) / period
+    if t < 0.25:
+        return 2 * amplitude * t / period + dc_offset
+    elif t < 0.75:
+        return -2 * amplitude * (t / period - 0.5) + dc_offset
+    else:
+        return 2 * amplitude * (t / period - 1) + dc_offset
+
+
+x = np.linspace(0, 1, 1000000)
+y = []
+for t in x:
+    y.append(impulse(10e10, 10e-6, 0, t))
+
+plt.plot(x, y)
+plt.show()
